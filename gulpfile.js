ac9b5d7d1fddc7ef54ed9e7bca687ae0139bcd7b
Jeremy Yen <jeremy5189@gmail.com>
2016-01-03 18:12:34 +0800

    Complete gulp file

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    gulpCopy = require('gulp-file-copy');
+
+gulp.task('copy', function() {
+  gulp.src('./bower_components/jquery/jquery.min.js')
+      .pipe(gulp.dest('./js/lib/'));
+
+  gulp.src('./jquery-mobile/jquery.mobile-1.4.5.js')
+       .pipe(gulp.dest('./js/lib/'));
+
+  gulp.src('./bower_components/moment/min/moment-with-locales.min.js')
+       .pipe(gulp.dest('./js/lib/'));
+
+  gulp.src('./bower_components/local-storage/src/LocalStorage.js')
+       .pipe(gulp.dest('./js/lib/'));
+
+  gulp.src('./jquery-mobile/jquery.mobile-1.4.5.css')
+       .pipe(gulp.dest('./css/lib/'));
+});
