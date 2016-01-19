f0584a8a1eb562b1091edca140863081f4c1da83
Jeremy Yen <jeremy5189@gmail.com>
2016-01-19 23:17:46 +0800

    Add relative time

---
+++ js/app-moment.js
@@ -0,0 +1,17 @@
+moment.locale('zh-tw', {
+    relativeTime : {
+        future: "%s後",
+        past:   "%s前",
+        s:  "%d 秒",
+        m:  "1 分",
+        mm: "%d 分",
+        h:  "1 時",
+        hh: "%d 時",
+        d:  "1 天",
+        dd: "%d 天",
+        M:  "1 月",
+        MM: "%d 月",
+        y:  "1 年",
+        yy: "%d 年"
+    }
+});
